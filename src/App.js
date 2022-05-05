9fb1c4ce8298d1983f9c33f7d6fa432b702d166d
ihsanfansyuri <67683530+ihsanfansyuri@users.noreply.github.com>
2022-05-05 23:21:57 +0800

    updating file

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import SearchIcon from "./search.svg";
 // 22775245
 
 const API_URL = "https://www.omdbapi.com/?apikey=22775245";
-// const API_URL = "https://api.themoviedb.org/3";
-// const API_KEY = "be42a911c47a690bd54e2e9f1e0a831d";
 
 // const movie1 = {
 //   Title: "Amazing Spiderman Syndrome",
@@ -24,9 +22,6 @@ const App = () => {
 
   const searchMovie = async (title) => {
     const res = await fetch(`${API_URL}&s=${title}`);
-    // const res = await fetch(
-    //   `${API_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${title}&page=1&include_adult=false`
-    // );
     const data = await res.json();
 
     setMovies(data.Search);
